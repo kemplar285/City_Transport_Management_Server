ad9416c00dc99ba7d09f80602ccdd731ddd8117b
e8e4caivtw <kemplar285@gmail.com>
2022-04-08 12:38:08 +0300

    First commit. Code generated.

---
+++ src/main/java/ee/cybernetica/citytransport/CitytransportApplication.java
@@ -0,0 +1,13 @@
+package ee.cybernetica.citytransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitytransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitytransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/cybernetica/citytransport/CitytransportApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.cybernetica.citytransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitytransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
